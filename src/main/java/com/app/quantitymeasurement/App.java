4dbf5f865c9f0b6b87a7d63659ef51c6c37dfc59
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-02-23 12:39:05 +0530

    [Saurabh Wanjari] Initial setup

---
+++ src/main/java/com/app/quantitymeasurement/App.java
@@ -0,0 +1,14 @@
+package com.app.quantitymeasurement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello saurabh ");
+    }
+}
