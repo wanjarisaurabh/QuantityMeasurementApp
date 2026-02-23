a6ba46eee04d7f17c099dbc11689656ae41d59a5
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-02-23 15:12:12 +0530

    [saurabh wanjari] Add . Added QuantityMeasurementApp Feet and Inches

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,8 +2,7 @@ package com.app.quantitymeasurement.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 	
-
-    public static class Feet {
+	public static class Feet {
     	//final varible
         private final double value;
 
@@ -24,10 +23,54 @@ public class QuantityMeasurementApp {
         }
     }
 
+   
+    
+    
+    //Inches Inner classs for inch operations
+    public static class Inches {
+    	//final varible
+        private final double value;
+
+        //constructor
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+        	//checking object
+            if (this == obj) return true;
+            //checking if objects of same class 
+            if (obj == null || getClass() != obj.getClass()) return false;
+            //converting now
+            Inches feet = (Inches) obj;
+            return Double.compare(feet.value, this.value) == 0;
+        }
+    }
+    
+    //method to represent feet equality
+    
+    public static void demonstrateFeetEquality() {
+    	Feet feet1=new Feet(1.0);
+    	Feet feet2=new Feet(1.0);
+    	System.out.println("Input: 1.0 ft and 1.0 ft");
+    	System.out.println("Output: Equal ("+feet1.equals(feet2)+")");
+    }
+    
+    //method to represent inch equality
+    
+    public static void demonstrateInchesEquality() {
+    	Inches inch1=new Inches(1.0);
+    	Inches inch2=new Inches(1.0);
+    	System.out.println("Input: 1.0 ft and 1.0 ft");
+    	System.out.println("Output: Equal ("+inch1.equals(inch2)+")");
+    }
+    
+    
+    //The main method
     public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-        System.out.println("Equal: " + feet1.equals(feet2));
+        demonstrateFeetEquality();
+        demonstrateFeetEquality();
     }
 
 }
