58000d0e1e86032b84845303e91a2dd6cb84a9f4
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-03-17 11:42:08 +0530

    [saurabh wanjari] Add . Added Necessary files

--- src/main/java/com/app/quantitymeasurement/units/SupportsArithmetic.java
+++
@@ -1,9 +0,0 @@
-package com.app.quantitymeasurementapp.units;
-
-/**
- * UC14/UC15: Functional Interface to dictate if a unit supports arithmetic.
- */
-@FunctionalInterface
-public interface SupportsArithmetic {
-    boolean isSupported();
-}
